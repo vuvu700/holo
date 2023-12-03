d1019e91d56a51837764ef2613d39b3a95335017
vuvu700 <vuvu700.vuvu@gmail.com>
2023-12-03 14:46:27 +0100

    complet rework of parallel.py

--- parallel.py
+++ parallel.py
@@ -1,316 +1,94 @@
+from collections.abc import Callable, Iterable, Mapping
 from queue import Queue, Empty as EmptyError
 from threading import Thread
 import time
-from typing import Callable, Any, Iterable
-from array import array
 
+from holo.__typing import Callable, Any, Iterable, TypeAlias
+from holo.protocols import _T, _P
 from holo import Pointer
 
-
-# TODO: ajouter un moyen de clear les outputs deja calculés (seulement possible si )
-
+_TaskNoReturn:TypeAlias = "tuple[Callable[..., None], Iterable[Any], dict[str, Any]]"
+_TaskWithReturn:TypeAlias = "tuple[Callable[..., Any], Iterable[Any], dict[str, Any]]"
 
 class Worker(Thread):
-    """a worker that will exec tasks one by one, and send the anwser back"""
-
-    def __init__(self,
-            workFunction:"Callable[[Any], Any|None]",
-            queueInputs:"Queue[Any]",
-            queueOutputs:"Queue[tuple[Any, Any]]",
-            verbose:int=1, getItemTimeout:"float|None"=None,
-            stopWhenOutOfWork:bool=False,
-            p_workDoneCounter:"Pointer[int]|None"=None)->None:
-        """ * `workFunction` is the function that wil be called each time, it arg will be the value from queueInputs, \
-                it will retun a result or None, None mean no result will be added to the queue\n
-            * `queueInputs` is the queue where the inputs will be getted\n
-            * `queueOutputs` is where the outputs will be added, in a tuple like: tuple(input, output)\n
-            * `getItemTimeout` is the time in second before the next lookup on the queueInputs if empty or blocked,\
-                None mean no waiting time, None or a value too low migth compromise the \n
-        """
-        super().__init__()
-
-        self.workFunction:"Callable[[Any], Any|None]" = workFunction
-        self.queueInputs:"Queue[Any]" = queueInputs
-        self.queueOutputs:"Queue[tuple[Any, Any]]" = queueOutputs
-
-        self.verbose:int = verbose
-        self.getItemTimeout:"float|None" = getItemTimeout
-        self.__running:bool = False
-        self.__stoped:bool = False
-        self.__waitingInputs:bool = False
-        self.__getItemBlock:bool = not isinstance(self.getItemTimeout, type(None))
-        self.__stopWhenOutOfWork:bool = stopWhenOutOfWork
-        self.__p_workDoneCounter:"Pointer[int]|None" = p_workDoneCounter
-
-    def run(self):
-        while self.__running is True:
-
-            try:
-                arg:"Any" = self.queueInputs.get(block=self.__getItemBlock, timeout=self.getItemTimeout)
-                self.__waitingInputs = False
-            except EmptyError:
-                if self.__stopWhenOutOfWork is True:
-                    self.stop()
-                else:
-                    self.__waitingInputs = True
+    def __init__(self, manager:"Manager")->None:
+        super().__init__(daemon=True)
+        self.manager = manager
+        
+    def run(self)->None:
+        while True:
+            # handle pausing the workers
+            if self.manager.isPaused is True:
+                time.sleep(1e-3) # => sleep and relase the thread
                 continue
-
-            try:
-                result:"Any" = self.workFunction(arg)
-                if result is not None:
-                    self.queueOutputs.put((arg, result))
-
-            except Exception as error:
-                if self.verbose == 1:
-                    print(f"{type(error)} happend for the value: {arg}")
-                elif self.verbose > 1:
-                    print(f"{error}, for the value: {arg}")
-
-            finally:
-                if self.__p_workDoneCounter is not None:
-                    self.__p_workDoneCounter.value += 1
-                self.queueInputs.task_done()
-        
-        if (self.__running is True) or (self.__stoped is True):
-            raise RuntimeError(
-                "bad dev: __running and __stoped should be false when exiting the run, "
-                + f"but __running={self.__running} and __stoped={self.__stoped}")
-        self.__waitingInputs = False
-        
-    def start(self)->bool:
-        """try to start the Worker for the first time, return True it started the Worker, false otherwise"""
-        if self.__running is True:
-            return False
-        elif self.__stoped is True:
-            return False
-        else:
-            self.__running = True
-            super().start()
-            return True
-
-    def stop(self)->bool:
-        """try to stop the Worker for the first time, return True it stopped the Worker, false otherwise"""
-        if self.__running is False:
-            return False
-        elif self.__stoped is True:
-            return False
-        else:
-            self.__running = False
-            self.__stoped = False
-            return True
-
-    def isWaitingInputs(self)->bool:
-        return self.__waitingInputs
-
-    def isRunning(self)->bool:
-        return self.__running
-
-    def isStoped(self)->bool:
-        return self.__stoped
-
-
-class WorkersPool:
-    """pool of Workers doing multiple instances of a single task in parallel"""
-
-
-    def __init__(self,
-            numberOfWorkers:int,
-            workFunction:"Callable[[Any], Any|None]",
-            initialInputs:"Iterable[Any]|None"=None,
-            verbose:int=1, workersGetItemTimeout:"float|None"=None,
-            stopWorkersWhenOutOfWork:bool=False, trackWorkProgress:bool=True)->None:
-        
-        if numberOfWorkers <= 0:
-            raise ValueError(f"the number of workers need to be >= 1")
-        
-        # vars setup
-        self.nbWorkers:int = numberOfWorkers
-        self.verbose:int = verbose
-        self.workersGetItemTimeout:"float|None" = workersGetItemTimeout
-        self.stopWorkersWhenOutOfWork:bool = stopWorkersWhenOutOfWork
-        self.__addedWorkCount:int = 0
-        self.__finishedWorkCount:int = 0
-        self.__finishedWorkCount_perWorker:"list[Pointer[int]]" = [Pointer(0) for _ in range(self.nbWorkers)]
-        self.__trackWorkProgress:bool = trackWorkProgress
-
-        # create the queues needed
-        self.queueInputs:"Queue[Any]" = Queue(0)
-        self.queueOutputs:"Queue[tuple[Any, Any]]" = Queue(0)
-        self.__outputsConverted:"list[list[tuple[Any, Any]]]" = []
-        """all the outputs previously converted"""
-
-        # create all the workers
-        self.__workersList:"list[Worker]" = []
-        for workerIndex in range(self.nbWorkers):
-
-            if self.__trackWorkProgress is True:
-                p_workDoneCounter = self.__finishedWorkCount_perWorker[workerIndex]
-            else:p_workDoneCounter = None
-
-            self.__workersList.append(
-                Worker(
-                    workFunction, self.queueInputs, self.queueOutputs,
-                    verbose=self.verbose, getItemTimeout=self.workersGetItemTimeout,
-                    stopWhenOutOfWork=self.stopWorkersWhenOutOfWork,
-                    p_workDoneCounter=p_workDoneCounter,
-                )
-            )
-
-        # add the initial inputs
-        if initialInputs is not None:
-            self.addInputs(initialInputs)
             
+            # try to get some work
+            try: (func, funcArgs, funcKwargs) = self.manager._tasksList.get()
+            except EmptyError: 
+                time.sleep(1e-5) # => relase the thread
+                continue # => retry
+            
+            #=> got some work => do it
+            func(*funcArgs, **funcKwargs)
+            self.manager._tasksList.task_done()
+
+
+class Manager():
+    def __init__(self, nbWorkers:int, startPaused:bool=False)->None:
+        self._tasksList:"Queue[_TaskNoReturn]" = Queue()
+        self.__workers:"list[Worker]" = [Worker(self) for _ in range(nbWorkers)]
+        self.__paused:bool = startPaused
+        # start the workers
+        for worker in self.__workers:
+            worker.start()
+
+    def addWork(self, func:"Callable[_P, None]", *funcArgs:_P.args, **funcKwargs:_P.kwargs)->None:
+        """add the work to the stack, will start when a worker will be available\n
+        if you need to return a value, consider callbacks, or use a Pointer"""
+        self._tasksList.put((func, funcArgs, funcKwargs))
     
-    def addInputs(self, newInputs:"Iterable[Any]")->None:
-        """add the new work to the current queue"""
-        addedWorkCount:int = 0
-        for inputArg in newInputs:
-            self.queueInputs.put(inputArg)
-            addedWorkCount += 1
-        if self.__trackWorkProgress is True:
-            self.__addedWorkCount += addedWorkCount
-
-    def startWorkers(self)->bool:
-        """strat all workers, and return True if it started them all"""
-        return all([worker.start() for worker in self.__workersList])
-
-    def stopWorkers(self)->bool:
-        """stop all workers, and return True if it stoped them all"""
-        return all([worker.stop() for worker in self.__workersList])
-
-    def joinWorkers(self)->bool:
-        """if the workers will stop when out of work, return True when all workers joined\n
-        if the workers will NOT stop when out of work, return False\n"""
-        if self.stopWorkersWhenOutOfWork is True:
-            for worker in self.__workersList:
-                worker.join()
-            return True
-        else:
-            return False
-
-    def joinWorkFinished(self)->bool:
-        """join when all the planned work is finished and return True or no workers are working and return False"""
-        intervalChecksWorkers:float = 1e-4 #sec
-        with self.queueInputs.all_tasks_done:
-
-            # while some unfinished tasks remain
-            while self.queueInputs.unfinished_tasks != 0:
-                # test if no worker are working
-                if sum(worker.isRunning() is True for worker in self.__workersList) == 0:
-                    return False
-                #wait a certaint time before re checking the workers 
-                self.queueInputs.all_tasks_done.wait(intervalChecksWorkers)
-                
-        return True
-
-    def isWorkFinished(self)->bool:
-        """return whether all the planed work is finished"""
-        with self.queueInputs.all_tasks_done:
-            return self.queueInputs.unfinished_tasks == 0
-
-    def workersAreRunning(self)->bool:
-        """return True if any worker is still running"""
-        return any(worker.isRunning for worker in self.__workersList)
-
-
-    def getLastOutputs(self)->"list[tuple[Any, Any]]":
-        nbOutputs:int = self.queueOutputs.qsize()
-        # the real nb of outputs might increase but will not decrease outside of this function
-        outputsList:"list[tuple[Any, Any]]" = [(None, None)] * nbOutputs
-
-        nbOutputsConverted:int = 0
-        outputValue:"tuple[Any, Any]"
-        for index in range(nbOutputs):
-            try:
-                outputValue = self.queueOutputs.get(block=False)
-                outputsList[index] = outputValue
-                nbOutputsConverted += 1
-
-            except EmptyError:
-                outputsList = outputsList[: nbOutputsConverted]
-                break
-
-        if len(outputsList) > 0:
-            self.__outputsConverted.append(outputsList)
-
-        return outputsList
-
-    def getAllOutputs(self, updateOutputs:bool=True)->"list[tuple[Any, Any]]":
-        if updateOutputs is True:
-            self.getLastOutputs()
-        outputsList:"list[tuple[Any, Any]]" = []
-        for convertedList in self.__outputsConverted:
-            outputsList += convertedList
-        return outputsList
+    @property
+    def isPaused(self)->bool:
+        """whether the workers are able to start new tasks"""
+        return self.__paused
+    def unPause(self)->None:
+        """un pause the workers, do nothing if they alredy were not paused"""
+        self.__paused = False
+    def pause(self)->None:
+        """pause the workers, do nothing if they alredy were paused\n
+        (it won't stop any current tasks, but it will not launche new ones)"""
+        self.__paused = True
     
-        
-    def getAllOutputs_fast(self, updateOutputs:bool=True)->"list[list[tuple[Any, Any]]]":
-        if updateOutputs is True:
-            self.getLastOutputs()
-        return self.__outputsConverted
+    def isEmpty(self)->bool:
+        """return True when no more tasks are scheduled"""
+        return (self._tasksList.qsize() == 0) # better than .empty()
+    def join(self)->None:
+        """return when all tasks are finished"""
+        self._tasksList.join()
 
-    def getNbWorkAdded(self)->int:
-        """return the number of planned work"""
-        return self.__addedWorkCount
 
-    def getNbWorkFinished(self)->int:
-        """return the total cont of finished work"""
-        # update the count
-        new_finishedWorkCount:int = 0
-        for indexWorker in range(self.nbWorkers):
-            new_finishedWorkCount += self.__finishedWorkCount_perWorker[indexWorker].value
-        self.__finishedWorkCount = new_finishedWorkCount
-        
-        return self.__finishedWorkCount
+def taskResultGraber(func:"Callable[_P, None]", resPtr:Pointer, *funcArgs:_P.args, **funcKwargs:_P.kwargs)->None:
+    """a util function that put the result of `func` in `resPtr`"""
+    resPtr.value = func(*funcArgs, **funcKwargs)
+
+def parallelExec(tasks:"list[_TaskWithReturn]", nbWorkers:"int|None")->"list[Any]":
+    """execute the `tasks` in parallel with `nbWorkers` workers\n
+    `nbWorkers`: int -> the numbr of worker to create, None -> create one worker per tasks\n
+    return a list of results as [tasks[0] -> res[0], ..., tasks[n] -> res[n]]"""
+    nbTasks:int = len(tasks)
+    # create pointers to grab each results
+    resultPointers = [Pointer() for _ in range(nbTasks)]
+    # crate the manager
+    if nbWorkers is None:
+        manager = Manager(nbWorkers=nbTasks, startPaused=False)
+    else: manager = Manager(nbWorkers=nbWorkers, startPaused=False)
+    # start working
+    for taskIndex, (func, funcArgs, funcKwargs) in enumerate(tasks):
+        manager.addWork(taskResultGraber, func, resultPointers[taskIndex], *funcArgs, **funcKwargs)
+    # wait until all tasks are done
+    manager.join()
+    # assemble the results and return
+    return [resultPointers[taskIndex].value for taskIndex in range(nbTasks)]
     
-    def getNbWorkFinished_perWorker(self)->"list[int]":
-        """return a copy of the count of finished work, for each worker"""
-        return [ptr.value for ptr in self.__finishedWorkCount_perWorker]
 
-    def clearOutputs(self)->bool:
-        """try to clear the outputs and reset the counters of work, only possible if the work is finished"""
-        if self.isWorkFinished() is True:
-            self.__outputsConverted = []
-            self.__addedWorkCount = 0
-            self.__finishedWorkCount = 0
-            for indexWorker in range(self.nbWorkers):
-                self.__finishedWorkCount_perWorker[indexWorker].value = 0
-
-            return True
-        else:
-            return False
-
-def demo()->None:
-    ## run a demo
-    def func(t)->float:
-        t *= 1e-6
-        time.sleep(t)
-        return t
-
-    pool = WorkersPool(10, func, range(1_000))
-    print("pool created")
-    print(pool.getNbWorkFinished(), pool.getNbWorkAdded())
-    
-    pool.startWorkers()
-    print("pool started")
-
-    print(pool.getNbWorkFinished(), pool.getNbWorkAdded())
-    time.sleep(0.01)
-    print("slept 0.01 sec")
-    print(pool.getNbWorkFinished(), pool.getNbWorkAdded())
-    
-    pool.addInputs(range(1_000, 2_000))
-    print("added new work")
-
-    print(pool.getNbWorkFinished(), pool.getNbWorkAdded())
-    time.sleep(0.01)
-    print("slept 0.01 sec")
-    print(pool.getNbWorkFinished(), pool.getNbWorkAdded())
-
-    print("waiting all work are finished")
-    pool.joinWorkFinished()
-    print("all work are finished")
-
-    print(pool.getNbWorkFinished(), pool.getNbWorkAdded())
-    print(pool.getNbWorkFinished_perWorker())
 
