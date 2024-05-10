94b741f879482eef600716ff7696731e95ae6106
vuvu700 <vuvu700.vuvu@gmail.com>
2024-05-10 17:35:33 +0200

    imporvements on logger, might break compatibility if using _LoggerWriter

--- logger.py
+++ logger.py
@@ -1,7 +1,7 @@
 import sys
 import time
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime, timedelta
 from io import TextIOWrapper
 
 from holo.__typing import Union, TextIO, Literal
@@ -12,39 +12,33 @@ HOUR_TIME_FORMAT = "%H:%M:%S"
 
 _WritersNames = Literal['stderr', 'stdout']
 
-def isSameDate(date1:datetime, date2:datetime)->bool:
-    """return True if the two date are the same day"""
-    return (date1.day == date2.day) \
-        and (date1.month == date2.month) \
-        and (date1.year == date2.year)
-
-floatToDtime = datetime.fromtimestamp
 
 class _LoggerWriter(TextIO):
-    intervaleInfoText:float = 0.001
-    
-    def __init__(self, logFile:"TextIO", copyFile:"TextIO|None", level:str)->None:
+    def __init__(self, logFile:"TextIO", copyFile:"TextIO|None", level:str, 
+                 newLogLineAfter:"timedelta"=timedelta(milliseconds=1))->None:
         self.logFile:"TextIO" = logFile
         self.copyFile:"TextIO|None" = copyFile
         self.level:str = level
-        self.__timeLastWrite:float = 0.0
-        self.__timeLastInfos:float = 0.0
+        self.__timeLastWrite: datetime = datetime.min
+        self.__timeLastInfos: datetime = datetime.min
+        self.newLogLineAfter: timedelta = newLogLineAfter
 
-    @property
-    def timeSinceLastWrite(self)->float:
-        return time.time() - self.__timeLastWrite
-    @property
-    def timeSinceLastInfos(self)->float:
-        return time.time() - self.__timeLastInfos
+    def timeSinceLastWrite(self)->timedelta:
+        return datetime.now() - self.__timeLastWrite
+
+    def timeSinceLastInfos(self)->timedelta:
+        return datetime.now() - self.__timeLastInfos
     
     def writeLogsInfos(self)->None:
-        currentTime:float = time.time()
+        currentTime: datetime = datetime.now()
         # determine logs infos to write
         
-        if (self.timeSinceLastWrite >= self.intervaleInfoText) or (self.timeSinceLastInfos > 1.0):
-            # select the format for the 
+        if (self.timeSinceLastWrite() >= self.newLogLineAfter) \
+                or (self.timeSinceLastInfos() > timedelta(seconds=1)):
+            # needs to write new logs infos
+            # select the format for the time
             timeFormat:str
-            if isSameDate(floatToDtime(self.__timeLastInfos), floatToDtime(currentTime)) is False:
+            if self.__timeLastInfos.date() == currentTime.date() is False:
                 timeFormat = FULL_TIME_FORMAT
             else: timeFormat =  HOUR_TIME_FORMAT
             
@@ -69,16 +63,16 @@ class _LoggerWriter(TextIO):
 
 class Logger():
     def __init__(self,
-            filePath:"StrPath", encoding:"str|None"=None,
-            _noRePrint:"bool"=False)->None:
+            filePath:"StrPath", encoding:"str|None"=None, _noRePrint:"bool"=False, 
+            newLogLineAfter:"timedelta"=timedelta(milliseconds=1))->None:
         aux = lambda var, cond: None if cond else var
         self.file:"TextIO|None" = None # to avoid more errors in __del__
         self.file = open(filePath, mode='a', encoding=encoding)
         self.file.seek(0, 2) # got to the end of the log file
-        self.logStdout:"_LoggerWriter" = \
-            _LoggerWriter(self.file, aux(sys.stdout, _noRePrint) , "INFO")
-        self.logStderr:"_LoggerWriter" = \
-            _LoggerWriter(self.file, aux(sys.stderr, _noRePrint), "ERROR")
+        self.logStdout:"_LoggerWriter" = _LoggerWriter(
+            self.file, aux(sys.stdout, _noRePrint) , "INFO", newLogLineAfter)
+        self.logStderr:"_LoggerWriter" = _LoggerWriter(
+            self.file, aux(sys.stderr, _noRePrint), "ERROR", newLogLineAfter)
 
         sys.stderr = self.logStderr
         sys.stdout = self.logStdout
@@ -105,11 +99,13 @@ class Logger():
 
 _Encoding = str
 class LoggerContext():
-    def __init__(self, file:"StrPath|tuple[StrPath, _Encoding]|TextIOWrapper")->None:
+    def __init__(self, file:"StrPath|tuple[StrPath, _Encoding]|TextIOWrapper", 
+                 newLogLineAfter:"timedelta"=timedelta(milliseconds=1))->None:
+        self.newLogLineAfter: timedelta = newLogLineAfter
         self.file:TextIO
         if isinstance(file, TextIOWrapper):
             self.file = file
-        else:
+        else: # => 
             encoding:"str|None" = "utf-8" # don't use None to be consistant with the std...
             if isinstance(file, tuple):
                 (file, encoding) = file
@@ -121,9 +117,9 @@ class LoggerContext():
         if (self.logStdout is not None) or (self.logStderr is not None):
             raise RuntimeError("the self.logStdout or self.logStderr is still opened")
         self.file.seek(0, 2) # got to the end of the log file
-        self.logStdout = _LoggerWriter(self.file, sys.stdout , "INFO")
+        self.logStdout = _LoggerWriter(self.file, sys.stdout , "INFO", self.newLogLineAfter)
         sys.stdout = self.logStdout
-        self.logStderr = _LoggerWriter(self.file, sys.stderr, "ERROR")
+        self.logStderr = _LoggerWriter(self.file, sys.stderr, "ERROR", self.newLogLineAfter)
         sys.stderr = self.logStderr
     
     def __exit__(self, *_, **__)->None:
