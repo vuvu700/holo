56990cee1c2c7f21953003c00816c36beffe7a8d
vuvu700 <vuvu700.vuvu@gmail.com>
2025-06-24 21:06:02 +0200

    made compatible with python pip system

---
+++ README.md
@@ -0,0 +1,5 @@
+# Read me
+
+this is my personnal tools package.
+when there is a tool that migth be usefull to have across all my programs i add it here.
+NOTE: there is currently no compatibility that is ensured betwin each versions
--- setup.py
+++ setup.py
@@ -1,84 +1,30 @@
-from __future__ import annotations
-from pathlib import Path
-from setuptools import setup, Extension
-from Cython.Build import cythonize
-import os
-import sys
-import shutil
+from setuptools import setup, find_packages
 
-############## HELP ##############
-# (debug files remain) $python ./setup.py build_ext 
-# (only *.pyd/.so remain): $python -OO ./setup.py build_ext 
-
-
-DEP_FILES:bool; ANNOTATE:bool; FORCE:bool; DEBUG:bool
-OPTIMIZE:str; USE_ASSERTS:bool; DEL_GENERATED_FILES:bool
-
-if __debug__: # debug
-    DEP_FILES = True
-    ANNOTATE = True
-    FORCE = False
-    DEBUG = True
-    OPTIMIZE = ("/Od" if sys.platform == "win32" else "-O0")
-    USE_ASSERTS = True
-    DEL_GENERATED_FILES = False
-
-else: # release
-    DEP_FILES = False
-    ANNOTATE = False
-    FORCE = True
-    DEBUG = False
-    OPTIMIZE = ("/O2" if sys.platform == "win32" else "-O3")
-    USE_ASSERTS = False
-    DEL_GENERATED_FILES = True
-
-
-SRC_DIR = Path(".")
-modulesToCompile:dict[str, Path] = {
-    moduleName : \
-        SRC_DIR.joinpath(f"{moduleName}.pyx")
-    for moduleName in [
-        "pyx_datatypes", "pyx_reader",
-    ]
-}
-extra_compile_args:list[str] = [OPTIMIZE, ]
-if USE_ASSERTS is False:
-    extra_compile_args.append("-DCYTHON_WITHOUT_ASSERTIONS")
-
-ext_modules = [
-    Extension(
-        moduleName, [modulePath.as_posix()], 
-        include_dirs=["."], 
-        extra_compile_args=extra_compile_args
-    ) for (moduleName, modulePath) in modulesToCompile.items()
-]
 
 setup(
-    include_dirs=[SRC_DIR.as_posix()],
-    ext_modules = cythonize(
-        ext_modules, #modulesToCompile,
-        annotate=ANNOTATE, depfile=DEP_FILES, 
-        force=True, show_all_warnings=False, gdb_debug=DEBUG),
-    options={"build_ext": {"build_lib": SRC_DIR.as_posix()}},
+    name='holo',
+    version='0.1.0',
+    author='Ludovic Andrieu',
+    author_email='vuvu700.vuvu@gmail.com',
+    description='A short description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/vuvu700/holo',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "numpy",
+        "numba",
+        "matplotlib",
+        "typing_extensions",
+        "requests",
+        "mypy",
+        "multiprocess",
+        "ffmpeg-python",
+    ],
 )
-
-### clean the generated files
-if DEL_GENERATED_FILES is True:
-    #clean the *.[c, c.dep, html] files
-    print("cleaning *.c files")
-    for (moduleName, modulePath) in modulesToCompile.items():
-        for extention in (".c", ".c.dep", ".html"):
-            moduleGeneratedFile:str = \
-                modulePath.as_posix().replace(".pyx", extention)
-            if os.path.exists(moduleGeneratedFile):
-                os.remove(moduleGeneratedFile)
-
-
-    # clean the build and debug
-    print("cleaning build dir")
-    if os.path.exists("./build"):
-        shutil.rmtree("./build")
-    
-    print("cleaning debug dir")
-    if os.path.exists("./cython_debug"):
-        shutil.rmtree("./cython_debug")
